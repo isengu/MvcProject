6ac341a0f222bbfaf54a220daef20903d32063d3
ismail Ensar GULSEN <ismail_ensar.gulsen@toyotatr.com>
2023-12-08 16:16:27 +0300

    Admin home page created

---
+++ MvcProject/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MvcProject.Data;
+using MvcProject.DTO;
+using MvcProject.Models;
+using System.Security.Claims;
+
+namespace MvcProject.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public AdminController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
